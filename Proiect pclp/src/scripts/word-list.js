35facceea243b04e17867f88525c15e1cae95d30
Botoi Ariana Mihaela <151063067+B-Ariana@users.noreply.github.com>
2024-01-09 23:30:12 +0200

    Add files via upload

---
+++ Proiect pclp/src/scripts/word-list.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "carpati",
+        hint: "Aceste masive montane se întind pe o lungime considerabilă în centrul țării și oferă peisaje spectaculoase."
+    },
+    {
+        word: "bran",
+        hint: "Acest castel, situat în Munții Carpați, este cunoscut și sub numele de Castelul lui Dracula."
+    },
+    {
+        word: "delta",
+        hint: "Această arie protejată este unul dintre cele mai mari și mai bine conservate ecosisteme de tip delta din Europa."
+    },
+    {
+        word: "sibiu",
+        hint: "Orașul acesta găzduiește un important festival cultural numit Festivalul Internațional de Teatru de la ... ."
+    },
+    {
+        word: "transfagarasean",
+        hint: " Această șosea spectaculoasă traversează Munții Făgăraș și oferă vederi panoramice impresionante."
+    },
+    {
+        word: "bucuresti",
+        hint: "Capitala României, cunoscută pentru muzeele sale, arhitectura eclectică și viața de noapte vibrantă."
+    },
+    {
+        word: "voronet",
+        hint: "Această mănăstire din nordul Moldovei este faimoasă pentru frescele sale exterioare, denumite și Albastrul de Voroneț."
+    },
+    {
+        word: "scarisoara",
+        hint: "Această peșteră din Munții Apuseni adăpostește un ghețar veșnic și este una dintre cele mai mari peșteri de gheață din lume."
+    },
+    {
+        word: "peles",
+        hint: "O bijuterie arhitectonică situată în Sinaia, acest castel a fost construit în stil neorenașcentist și a fost folosit ca reședință regală."
+    },
+    {
+        word: "horezu",
+        hint: " Cunoscută și sub numele de Hurezi, această mănăstire este un exemplu remarcabil al artei brâncovenești și este inclusă în Patrimoniul Mondial UNESCO."
+    }
+];
